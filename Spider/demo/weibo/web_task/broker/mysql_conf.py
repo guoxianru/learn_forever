5893a6649696a2270ea4b32a1bebf75153a5d8d8
guoxianru <admin@addcoder.com>
2021-01-26 17:51:43 +0800

    新增坐标系转换算法JS，删除无用demo，更新依赖文件
    Signed-off-by: guoxianru <admin@addcoder.com>

--- Spider/demo/weibo/web_task/broker/mysql_conf.py
+++
@@ -1,7 +0,0 @@
-mysql = {
-    'host': 'rm-2zet559663ri285kjno.mysql.rds.aliyuncs.com',
-    'user': 'dever',
-    'password': '123qwe',
-    'charset': 'utf8mb4',
-    'database':'domino_web_task'
-}
